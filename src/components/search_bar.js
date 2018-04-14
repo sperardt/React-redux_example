a47b75fe917c8b32ab66ffa94cbc465b3cdd80d8
Sabrina Perardt <sabrinaperardt@DANs-MBP.hitronhub.home>
2018-04-13 17:01:20 -0700

    video list

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'; // its the same that const Component = React.Component;
+
+class SearchBar extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = { term: ''};
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+            value={this.state.term}
+            onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
+
+  }
+
+}
+
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBXOFOK16RFWuhHqJIhzJZCyJWfP2TS2bI';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
+      this.setState({ videos }); //this.setState({ videos: videos });
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  };
+
+}
+//Take this component's generated HTML and put it
+//on the page ( in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
